65f88156c666b4da39e6e952b9401b8e53b566f6
byeolchorong <dudzlove@gmail.com>
2025-08-11 17:54:23 +0900

    css수정

--- src/main/java/com/fairplay/service/WalletServiceImpl.java
+++ src/main/java/com/fairplay/service/WalletServiceImpl.java
@@ -13,37 +13,37 @@ public class WalletServiceImpl implements WalletService{
 	@Autowired
 	private WalletRepository walletRepository;
 	
-	// ✅ 새 항목 저장
+	// 새 항목 저장
 	@Override
 	public void save(Wallet wallet) {
 		walletRepository.save(wallet);
 	}
 	
-	// ✅ 특정 회원의 전체 내역 조회
+	// 특정 회원의 전체 내역 조회
 	@Override
 	public List<Wallet> findByMemberId(int memberId) {
 		 return walletRepository.findByMemberId(memberId);
 	}
 	
-	// ✅ 단일 항목 조회
+	// 단일 항목 조회
 	@Override
 	public Wallet findById(int id) {
 		return walletRepository.findById(id);
 	}
 	
-	// ✅ 항목 수정
+	// 항목 수정
 	@Override
 	public void update(Wallet wallet) {
 		walletRepository.update(wallet);
 	}
 	
-	// ✅ 항목 삭제
+	// 항목 삭제
 	@Override
 	public void delete(int id) {
 		walletRepository.delete(id);
 	}
 	
-	// ✅ 같은 품목명 기준, 구매처별 단가 비교
+	// 같은 품목명 기준, 구매처별 단가 비교
 	@Override
 	public List<Wallet> comparePriceByItemName(int memberId, String itemName) {
 		return walletRepository.comparePriceByItemName(memberId, itemName);
